eeee2a6fa6ae3ba410e10751c0545d72c517fd56
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-01-31 20:28:11 +0100

    fix(client): add console log

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,6 +8,7 @@ restartButton.addEventListener('click', function (e) {
 function reloadServerStatus() {
     fetch("/status").then(response => response.json()).then(data => {
         console.log(data)
+        console.log(data === "true")
         if (data === "true") {
             status.textContent = "Status: Active"
         } else {
